51830f429fd7d7e358431105448e8e4f572e261b
Amaan Pasha <Amaanpasha23@gmail.com>
2025-05-17 03:05:17 +0530

    new butterfly pattern is added over here

--- src/HollowRectangle.java
+++ src/HollowRectangle.java
@@ -30,8 +30,45 @@ public class HollowRectangle {
         }
     }
 
+
+    public static void ButterFly(int n){
+        //1st half
+     for (int i=1; i<=n; i++){
+         //stars
+         for (int j = 1; j<=i; j++){
+             System.out.print("*");
+         }
+         //spaces
+         for (int j=1; j<=2*(n-i); j++ ){
+             System.out.print(" ");
+         }
+         //stars
+         for (int j=1; j<=i; j++) {
+             System.out.print("*");
+         }
+         System.out.println();
+     }
+     //2nd half
+
+        for (int i=n; i>=1; i--){
+            //stars
+            for (int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces
+            for (int j=1; j<=2*(n-i); j++ ){
+                System.out.print(" ");
+            }
+            //stars
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String [] args){
-        HollowRectangle(4,5);
-        newnum(5);
+//        HollowRectangle(4,5);
+//        newnum(5);
+        ButterFly(5);
     }
 }
